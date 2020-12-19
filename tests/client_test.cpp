645ab00b67967721a094cfdb6189cdfb28e48deb
yalavrinenko <lavrinenko@phystech.edu>
2020-12-19 21:05:18 +0300

    add client tests

--- tests/client_test.cpp
+++ tests/client_test.cpp
@@ -21,34 +21,32 @@ public:
   tcp::socket &socket() { return socket_; }
 
   void start() {
-
-  socket_.async_read_some(boost::asio::buffer(data_, max_length),
-                          [this](auto v1, auto v2) {
-                            this->handle_read(v1, v2);
-                          });
+    sync_read_thread_ = std::async(std::launch::async, [this](){
+      this->sync_read();
+    });
   }
 
+  auto const& messages() const { return messages_; }
 private:
-  void handle_read(const boost::system::error_code &error, size_t bytes_transferred) {
-    if (!error) {
-      srp::ClientActionMessage message;
-      message.ParseFromArray(data_, bytes_transferred);
-      std::string str;
-      auto status = google::protobuf::util::MessageToJsonString(message, &str);
-      std::cout << "[" << error.message() << "]\t\t" << status.message() << ":\t" << str << std::endl;
 
-      socket_.async_read_some(boost::asio::buffer(data_, max_length),
-                              [this](auto v1, auto v2) {
-                                this->handle_read(v1, v2);
-                              });
-    } else {
-      delete this;
+  void sync_read(){
+    std::optional<srp::ClientActionMessage> message;
+    do{
+      message = srp::NetUtils::sync_read_proto<srp::ClientActionMessage>(socket_);
+      if (message) {
+        std::string str;
+        auto status = google::protobuf::util::MessageToJsonString(message.value(), &str);
+        std::cout << status.message() << ":\t" << str << std::endl;
+        messages_.emplace_back(message);
+      }
     }
+    while (message);
   }
 
   tcp::socket socket_;
   enum { max_length = 1024 };
-  char data_[max_length];
+  std::future<void> sync_read_thread_;
+  std::vector<std::optional<srp::ClientActionMessage>> messages_;
 };
 
 class server {
@@ -57,24 +55,23 @@ public:
     start_accept();
   }
 
+  auto& session_ptr() { return session_; }
+
 private:
   void start_accept() {
-    auto new_session = new session(io_service_);
-    acceptor_.async_accept(new_session->socket(), [this, new_session](auto v2){
-      this->handle_accept(new_session, v2);
+    session_ = std::make_unique<session>(io_service_);
+    acceptor_.async_accept(session_->socket(), [this](auto v2){
+      this->handle_accept(session_, v2);
     });
   }
 
-  void handle_accept(session *new_session, const boost::system::error_code &error) {
+  void handle_accept(std::unique_ptr<session> &new_session, const boost::system::error_code &error) {
     if (!error) {
       new_session->start();
-    } else {
-      delete new_session;
     }
-
-    start_accept();
   }
 
+  std::unique_ptr<session> session_;
   boost::asio::io_service &io_service_;
   tcp::acceptor acceptor_;
 };
@@ -119,6 +116,33 @@ BOOST_AUTO_TEST_CASE(CreateClient){
   ios.stop();
 
   BOOST_REQUIRE_NO_THROW(server_thread.get());
+
+
 }
+
+  BOOST_AUTO_TEST_CASE(ClientInitMessage){
+    boost::asio::io_service ios;
+
+    auto server_thread = std::async(std::launch::async, [&ios](){
+      server s(ios, 14488);
+      ios.run();
+    });
+
+    std::this_thread::sleep_for(1s);
+
+    Client client(14488);
+    auto base_session = srp::base_session::create_session(std::move(client.Socket()));
+    {
+      std::unique_ptr<srp::recording_client> rclient;
+      BOOST_REQUIRE_NO_THROW(rclient = srp::recording_client::from_base_session(std::move(base_session)));
+      rclient->init(48);
+      BOOST_REQUIRE_NO_THROW(rclient.reset());
+    }
+
+    std::this_thread::sleep_for(1s);
+    ios.stop();
+
+    BOOST_REQUIRE_NO_THROW(server_thread.get());
+  }
 
 BOOST_AUTO_TEST_SUITE_END();
